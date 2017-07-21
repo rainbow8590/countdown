c16f8bdd749124115ed30932cf3e27018fa799d1
rainbow8590 <rainbow8590@163.com>
2017-07-21 18:56:54 +0800

    倒计时

---
+++ README.md
@@ -0,0 +1,5 @@
+## 倒计时
+    实现功能：设置未来时间可以输出“剩余：XX天XX时XX分XX秒”
+## 使用方法
+    <script src="jQuery.countdown.js"></script>
+    $('demo').countdown({'time':'2012-11-1 00:00:00'})
---
+++ jQuery.countdown.js
@@ -0,0 +1,46 @@
+;(function($){
+        $.fn.extend({
+            countdown:function(options){
+                var defaults = {
+                    container: this
+                }
+                var str = '';
+                var option = $.extend({},defaults,options);
+                
+                run();
+                setInterval(run,1000)
+                function run(){
+                  
+                    //设置未来时间
+                    var Ftime = new Date(option.time);
+                    var FtimeMs = Ftime.getTime();
+                    //获取本地时间
+                    var Ltime = new Date();
+                    var LtimeMs = Ltime.getTime();
+
+                    if(FtimeMs < LtimeMs ){
+                        return;
+                    }
+
+                    //获取时间差的秒数
+                    var diff = (FtimeMs - LtimeMs)/1000;
+                    //获取时间差的天
+                    var day = Math.floor(diff / (24*60*60))
+                    //获取时间差的时
+                    var hour = parseInt(diff /(60*60) %24)
+                    //获取时间差的分
+                    var min = parseInt(diff/60%60);
+                    //获取时间差的秒
+                    var sec = parseInt(diff % 60);
+
+                    //处理个位数
+                    function getTwo(num){
+                        return num < 10? '0' + num : num
+                    }
+                    str = '剩余：'+getTwo(day)+'天'+getTwo(hour)+'时'+getTwo(min)+'分'+getTwo(sec)+'秒';
+                    $(option.container).html(str)
+                }
+            }
+        })
+
+    })(jQuery)
